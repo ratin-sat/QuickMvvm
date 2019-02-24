963fc53a87a625731ebb5ee3f03d63a5d449be26
Satit Ratinukulkit <ratin_sat@hotmail.com>
2019-02-24 17:27:48 +0900

    implement ObservableObject and DelegateCommand

---
+++ QuickMvvm/DelegateCommand.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace QuickMvvm
+{
+    public class DelegateCommand<T> : ICommand
+    {
+        private readonly Action<T> execute;
+
+        private readonly Func<T, bool> canExecute;
+
+        private static bool CanExcute(T parameter) => true;
+
+        public DelegateCommand(Action<T> execute, Func<T, bool> canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentException(nameof(execute));
+            this.canExecute = canExecute ?? CanExcute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (this.canExecute != null)
+                {
+                    CommandManager.RequerySuggested += value;
+                }
+            }
+
+            remove
+            {
+                if (this.canExecute != null)
+                {
+                    CommandManager.RequerySuggested -= value;
+                }
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute(TranslateParameter(parameter));
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(TranslateParameter(parameter));
+        }
+
+        private T TranslateParameter(object parameter)
+        {
+            var value = default(T);
+            if (parameter != null && typeof(T).IsEnum)
+            {
+                value = (T)Enum.Parse(typeof(T), (string)parameter);
+            }
+            else
+            {
+                value = (T)parameter;
+            }
+
+            return value;
+        }
+    }
+
+    public class DelegateCommand : DelegateCommand<object>
+    {
+        public DelegateCommand(Action execute, Func<bool> canExecute = null)
+            : base(obj => execute(), (canExecute == null) ? null : new Func<object, bool>(obj => canExecute()))
+        {
+        }
+    }
+}
---
+++ QuickMvvm/ObservableObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace QuickMvvm
+{
+    public abstract class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(
+            ref T field,
+            T value,
+            [CallerMemberName] string propertyName = "")
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, value))
+            {
+                field = value;
+                this.OnPropertyChanged(propertyName);
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
